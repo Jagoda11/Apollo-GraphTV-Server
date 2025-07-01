17a613aa0f70321d4a2d67b53992aefac9d53880
Jagoda11 <31731113+Jagoda11@users.noreply.github.com>
2025-07-01 19:45:05 +0200

    Fix Jest ESM support

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,22 @@
-module.exports = {
-  preset: 'ts-jest',
+export default {
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
+  extensionsToTreatAsEsm: ['.ts'],
   globals: {
     'ts-jest': {
+      useESM: true,
       tsconfig: 'tsconfig.json',
     },
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^\.\/index.js$': '<rootDir>/src/index.ts',
+  },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { useESM: true, tsconfig: 'tsconfig.json' }],
+    '^.+\\.[jt]s$': ['babel-jest', { presets: ['@babel/preset-env'] }],
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!(graphql-request)/)',
+  ],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
 };
